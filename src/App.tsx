a5e176bbaf3769a6d2050fdb9a2f65f3fffff863
iamgromov <iamgromov@gmail.com>
2023-12-12 19:34:19 +0300

    update converter logic

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,33 @@ export default function App() {
   const [input, setInput] = useState<string>('');
   const [output, setOutput] = useState<string>('');
 
+  function convert(value: string) {
+    const container = document.querySelector('.color-container') as HTMLElement
+    if (/([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(value)) {
+      const r = parseInt(value.substring(0, 2), 16);
+      const g = parseInt(value.substring(2, 4), 16);
+      const b = parseInt(value.substring(4, 6), 16);
+      
+      setOutput(`rgb (${r}, ${g}, ${b})`);
+      container.style.background = `#${value}`;
+      console.log(value);
+    } else {
+      setOutput(`Ошибка!`)
+      container.style.background = '#ff0000';
+    }
+  }
+
   const handlerChangeInput = (event: ChangeEvent<HTMLInputElement>): void => {
     const value: string = event.target.value.slice(1, 7);
     setInput(value);
-    let color;
-    if (value.length === 2) color = value + value + value;
-    if (value.length === 3) color = value + value;
-    if (value.length === 6) color = value;
-    if (!color) return;
-    
-    const a = parseInt(color.slice(0, 2), 16);
-    const b = parseInt(color.slice(2, 4), 16);
-    const c = parseInt(color.slice(4, 6), 16);
-    if (!a || !b || !c) return setOutput('Ошибка!')
-    setOutput(`rgb(${a}, ${b}, ${c})`);
+
+    if (value.length === 6)  {
+      convert(value);
+    }
   }
 
   return (
-    <div className='app' style={{backgroundColor: output}}>
+    <div className='app'>
       <div className="color-container">
         <input 
           className='color__input input' 
